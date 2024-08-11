86a1d362ed68de62367124266b4d6a4830da57da
middle-age666 <13482006874@139.cn>
2024-08-11 15:59:36 +0800

    Site updated: 2024-08-11 15:59:35

---
+++ js/baidutongji.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9e4e5344f9627267f5808dcb1ec344d9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
